874408c279c4d60b87fe77030ffc3acef7810753
Roger Howard <rogerhoward@gmail.com>
2016-05-04 08:17:24 -0700

    Georg

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-sudo apt-add-repository ppa:brightbox/ruby-ng
+# sudo apt-add-repository ppa:brightbox/ruby-ng
+# sudo apt-get update
+# sudo apt-get -y install build-essential git ruby2.0 ruby2.0-dev
+
+# sudo gem install bundler
+
+# cd /vagrant
+# bundle install
+
 sudo apt-get update
-sudo apt-get -y install build-essential git ruby2.0 ruby2.0-dev
+sudo apt-get install -y git git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+cd ~/
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+echo 'eval "$(rbenv init -)"' >> ~/.profile
 
-gem install bundler
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.profile
+source ~/.profile
 
-cd /vagrant
+rbenv install -v 2.2.3
+rbenv global 2.2.3
+# gem install bundler
+
+# cd /vagrant
 # bundle install
